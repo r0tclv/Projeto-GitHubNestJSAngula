2a40aef4a27bcd1ad5d37b90e376bdeca57600cd
r0tclv <mota-victor-emanuel@outlook.com.br>
2026-03-03 03:23:47 -0300

    alterado:  backend/src/users/users.service.ts

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+	constructor(
+		@InjectRepository(User)
+		private readonly repo: Repository<User>,
+	) {}
+
+	async create(email: string, password: string) {
+		const hashed = await bcrypt.hash(password, 10);
+
+		return this.repo.save({
+			email,
+			password: hashed,
+		});
+	}
+}
